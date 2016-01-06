7c79b9588df05923a56d484a35ce9cdea271e636
gunsha <gonzaller@gmail.com>
2016-01-06 11:25:58 -0300

    First updates
    added support for multiple languages (jsgantt.lang.js)
    added support for parsing JSON tasks (JSGantt.parseJSON)
    some styles modifications

---
+++ js/jsgantt.lang.js
@@ -0,0 +1,16 @@
+/**
+ * Localization for JSGantt
+ * 2016 Gonzalo Aller
+ */
+
+var vLang = {"es":{
+	time : {hour:"Hora",hours:"Horas",minute:"Minuto",minutes:"Minutos",day:"Día",days:"Días"},
+	months : new Array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"),
+	tableHead : {resource:"Recurso",duration:"Duración",comp:"% Comp.",sDate:"Fecha Inicio",eDate:"Fecha Fin"}
+	},
+	"en":{
+		time : {hour:"Hour",hours:"Hours",minute:"Minute",minutes:"Minutes",day:"Day",days:"Days"},
+		months : new Array("January","February","March","April","May","June","July","August","September","October","November","December"),
+		tableHead : {resource:"Resource",duration:"Duration",comp:"% Comp.",sDate:"Start Date",eDate:"End Date"}
+	}
+};
